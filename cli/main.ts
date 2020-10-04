4e850a491408999cc93ca84ae5ff7903424454fd
Máron <maron.franca@gmail.com>
2020-10-04 12:32:34 -0300

    import transform stream

--- cli/main.ts
+++ cli/main.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import { ConvertToTypescriptTransform } from "./typescript/ConvertToTypescript.transform";
+import { ImportTransform } from "./typescript/Import.transform";
 
 const ARGS = process.argv.slice(2);
 const READ_FILE_PATH_AND_NAME = ARGS[0];
@@ -10,9 +11,11 @@ function main() {
     const writableStream = fs.createWriteStream(WRITE_FILE_PATH_AND_NAME);
     
     const convertToTypescriptStream = new ConvertToTypescriptTransform();
+    const importStream = new ImportTransform();
 
     file
         .pipe(convertToTypescriptStream)
+        .pipe(importStream)
         .pipe(writableStream);
 }
 main();
---
+++ cli/typescript/Import.transform.ts
@@ -0,0 +1,19 @@
+
+import { Transform, TransformCallback, TransformOptions } from "stream";
+
+const IMPORT_LIST = [
+    'import { Observable } from "rxjs";',
+] as const;
+
+export class ImportTransform extends Transform {
+    constructor(options?: TransformOptions) {
+        super(options);
+    }
+
+    _transform(chunk: any, _encoding: BufferEncoding, _callback: TransformCallback): void {
+        let fileText = chunk.toString();
+        const textWithValidation = IMPORT_LIST.join("\n") + "\n\n" + fileText;
+        const convertedChunk = Buffer.from(textWithValidation);
+        this.push(convertedChunk);
+    }
+}
