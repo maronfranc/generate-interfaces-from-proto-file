29b1e052988405a9c5aee4895aece1e6f90982e9
Máron <maron.franca@gmail.com>
2020-10-11 19:29:43 -0300

    replace import

--- cli/typescript/ConvertToTypescript.transform.ts
+++ cli/typescript/ConvertToTypescript.transform.ts
@@ -8,7 +8,7 @@ const replaceProtoToTypescript = (str: string): string => str
     // erases proto file syntax
     .replace("syntax = \"proto3\";", "")
     .replace("package checkout;", "")
-    .replace(/rpc\s*(\w+)\s*\((\w+)\)\s*returns\s*\((\w+)\);/g, "$1: ($2: $2) => Observable<$3>;")
+    .replace(/rpc\s*(\w+)\s*\((\w+)\)\s*returns\s*\((\w+)\);/g, "$1: ($2: $2, metadata?: Metadata) => Observable<$3>;")
     // change to lowercase function name e param
     .replace(/(\w+):\s*\((\w+)\s*:\s*(\w+)\)\s*=>\s*Observable<(\w+)>;/g, "->L<-$1: (->L<-$2: $3) => Observable<$4>;")
     .replace(/->L<-(\w+)/g, (_, word: string): string => word[0].toLowerCase() + word.substring(1))
@@ -16,8 +16,6 @@ const replaceProtoToTypescript = (str: string): string => str
     .replace(/service/g, "export class")
     .replace(/message/g, "export interface")
     .replace(/enum/g, "// TODO: enum convertion\nexport enum")
-    // add [] when starts with repeated word
-    .replace(/repeated\s+(\w+)\s+(\w+):?\w*?\s(=\s*\d+|\w+)/g, "$2: $1[]")
     // changes words order
     .replace(groupTypeAndVariableName, "$2: $1")
     // convert snake_case to camelCase
@@ -25,6 +23,10 @@ const replaceProtoToTypescript = (str: string): string => str
     // replace incorrects type names
     .replace(/bool/g, "boolean")
     .replace(rxAllNumberTypes, "number")
+    .replace(/\w+\.(\w+):/g, "$1:")
+    .replace(/import\s+(".+");/g, "")
+    // add [] when starts with repeated word
+    .replace(/repeated\s+(\w+):?\w*?\s(=\s*\d+|\w+)/g, "$2: $1[]")
     // Failed enum attempts
     // .replace(/enum\s+(\w+)\s*{(\s*(\w+)\s*=\s*(\d+);\s*)+\}/g, "enum $1 {\n\t$2 = $3,\n}")
     // .replace(/enum\s*(\w+)\s*{\n(?:\s*(\w+)\s*=\s*(\d+);)+\s*\n\s*}/, "enum $1 {\n\t$2 = $3,\n}")
